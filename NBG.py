c32e610da3e8156106ced6cc83842e1984ae4dac
mikheilTchulukh <124434845+mtchulukhadze@users.noreply.github.com>
2025-02-24 23:38:08 +0400

    NBG.py

--- NBG.py
+++ NBG.py
@@ -26,3 +26,31 @@ for i in data1:
             print(data[0], data[1], datetime.datetime.strptime(data[2], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%y-%m-%d'), datetime.datetime.strptime(data[3], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%y-%m-%d'))
 
 
+# html
+
+xml_response = requests.get(url_xml)
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+bs = BeautifulSoup(xml_response.text, 'html.parser')
+rows = bs.find_all('description')
+
+
+dataftame = []
+
+for row in rows:
+    html_content = row.text.strip()
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    table_rows = soup.find_all('tr')
+    for table_row in table_rows:
+        cells = table_row.find_all('td')
+
+        row_data = [
+            cells[0].text.strip(),
+            cells[2].text.strip()
+        ]
+        dataftame.append(row_data)
+
+print(pd.DataFrame(dataftame))
